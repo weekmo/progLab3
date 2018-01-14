1fc586176e5c83abb444ed6c1571b2445659c84f
weekmo <weekom@aol.com>
2018-01-14 11:28:02 +0100

    last commit

---
+++ Handout11/Exercise2/src/main/java/exercise2/Exercise2.java
@@ -0,0 +1,13 @@
+package exercise2;
+
+/**
+ * Hello world!
+ *
+ */
+public class Exercise2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Handout11/Exercise3/src/main/java/exercise3/Exercise3.java
@@ -0,0 +1,13 @@
+package exercise3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Exercise3 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
